087f8eb6c44bcac34f277b494730d31ffa3be369
MatshepoFrak <matshepo.soto@fraktionaldev.com>
2024-09-30 08:25:52 +0200

    partners

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// Add any necessary JavaScript for interactivity
+document.addEventListener('DOMContentLoaded', function() {
+    // You can add any JavaScript functionality here
+    console.log('Page loaded');
+});
